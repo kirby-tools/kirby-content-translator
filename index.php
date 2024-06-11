3c6487607033e4ba98cad0970f5f88929a286387
Johann Schopplich <mail@johannschopplich.com>
2024-06-11 17:53:53 +0200

    feat: `translator` global function and methods

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 use Composer\Semver\Semver;
 use Kirby\Cms\App as Kirby;
+use Kirby\Cms\File;
+use Kirby\Cms\Page;
+use Kirby\Cms\Site;
 use Kirby\Filesystem\F;
 
 F::loadClasses([
@@ -17,5 +20,27 @@ if (!Semver::satisfies(Kirby::version() ?? '0.0.0', '~4.0')) {
 Kirby::plugin('johannschopplich/content-translator', [
     'api' => require __DIR__ . '/src/extensions/api.php',
     'sections' => require __DIR__ . '/src/extensions/sections.php',
-    'translations' => require __DIR__ . '/src/extensions/translations.php'
+    'translations' => require __DIR__ . '/src/extensions/translations.php',
+    'siteMethods' => [
+        'translator' => function (array $options = []) {
+            return new \JohannSchopplich\ContentTranslator\Translator($this, $options);
+        }
+    ],
+    'pageMethods' => [
+        'translator' => function (array $options = []) {
+            return new \JohannSchopplich\ContentTranslator\Translator($this, $options);
+        }
+    ],
+    'fileMethods' => [
+        'translator' => function (array $options = []) {
+            return new \JohannSchopplich\ContentTranslator\Translator($this, $options);
+        }
+    ]
 ]);
+
+if (!function_exists('translator')) {
+    function translator(Site|Page|File $model, array $options = []): \JohannSchopplich\ContentTranslator\Translator
+    {
+        return new \JohannSchopplich\ContentTranslator\Translator($model, $options);
+    }
+}
