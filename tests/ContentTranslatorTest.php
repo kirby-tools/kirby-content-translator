be4655a8c358926131d63659a3928b74119d875d
Johann Schopplich <mail@johannschopplich.com>
2024-07-19 08:55:14 +0200

    test: remove unused import

--- tests/ContentTranslatorTest.php
+++ tests/ContentTranslatorTest.php
@@ -2,8 +2,6 @@
 
 declare(strict_types = 1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 use JohannSchopplich\ContentTranslator\Translator;
 use Kirby\Cms\App;
 use PHPUnit\Framework\TestCase;
