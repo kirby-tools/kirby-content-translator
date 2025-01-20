515a89ceb83cf554e7f17ea212d0d5d2b1565630
Johann Schopplich <mail@johannschopplich.com>
2025-01-20 11:20:19 +0100

    chore: add `es` language to playground

---
+++ playground/site/languages/es.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code' => 'es',
+    'default' => false,
+    'direction' => 'ltr',
+    'locale' => [
+        'LC_ALL' => 'es_ES'
+    ],
+    'name' => 'Español',
+    'translations' => [
+
+    ],
+    'url' => null
+];
