302d3e183130a0b871761515d6473352332c0e9d
Johann Schopplich <mail@johannschopplich.com>
2025-07-04 08:44:52 +0200

    chore: update playground

---
+++ playground/site/languages/fr.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code' => 'fr',
+    'default' => false,
+    'direction' => 'ltr',
+    'locale' => [
+        'LC_ALL' => 'fr_FR'
+    ],
+    'name' => 'Français',
+    'translations' => [
+
+    ],
+    'url' => null
+];
---
+++ playground/site/languages/zh.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code' => 'zh',
+    'default' => false,
+    'direction' => 'ltr',
+    'locale' => [
+        'LC_ALL' => 'zh_CN'
+    ],
+    'name' => '简体中文',
+    'translations' => [
+
+    ],
+    'url' => null
+];
