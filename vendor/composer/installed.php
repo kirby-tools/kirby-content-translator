d7afd74f8efa19da7b7be0fe1ddc6f911640c3f7
Johann Schopplich <mail@johannschopplich.com>
2025-10-15 08:22:22 +0200

    chore: release v3.8.1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'johannschopplich/kirby-content-translator',
-        'pretty_version' => '3.8.0',
-        'version' => '3.8.0.0',
+        'pretty_version' => '3.8.1',
+        'version' => '3.8.1.0',
         'reference' => null,
         'type' => 'kirby-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -20,8 +20,8 @@
             'dev_requirement' => false,
         ),
         'johannschopplich/kirby-content-translator' => array(
-            'pretty_version' => '3.8.0',
-            'version' => '3.8.0.0',
+            'pretty_version' => '3.8.1',
+            'version' => '3.8.1.0',
             'reference' => null,
             'type' => 'kirby-plugin',
             'install_path' => __DIR__ . '/../../',
