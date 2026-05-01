2329208ec13c1344fd56e9fe069afd29e4e71915
Johann Schopplich <johann@schopplich.com>
2026-05-01 21:43:16 +0200

    refactor: change DeepL class declaration to final

--- src/classes/ContentTranslator/DeepL.php
+++ src/classes/ContentTranslator/DeepL.php
@@ -10,7 +10,7 @@ use Kirby\Exception\LogicException;
 use Kirby\Http\Remote;
 use Kirby\Toolkit\A;
 
-class DeepL
+final class DeepL
 {
     public const SUPPORTED_SOURCE_LANGUAGES = ['AR', 'BG', 'CS', 'DA', 'DE', 'EL', 'EN', 'ES', 'ET', 'FI', 'FR', 'HE', 'HU', 'ID', 'IT', 'JA', 'KO', 'LT', 'LV', 'NB', 'NL', 'PL', 'PT', 'RO', 'RU', 'SK', 'SL', 'SV', 'TH', 'TR', 'UK', 'VI', 'ZH'];
     public const SUPPORTED_TARGET_LANGUAGES = ['AR', 'BG', 'CS', 'DA', 'DE', 'EL', 'EN', 'EN-GB', 'EN-US', 'ES', 'ES-419', 'ET', 'FI', 'FR', 'HE', 'HU', 'ID', 'IT', 'JA', 'KO', 'LT', 'LV', 'NB', 'NL', 'PL', 'PT', 'PT-BR', 'PT-PT', 'RO', 'RU', 'SK', 'SL', 'SV', 'TH', 'TR', 'UK', 'VI', 'ZH', 'ZH-HANS', 'ZH-HANT'];
@@ -49,9 +49,9 @@ class DeepL
         );
     }
 
-    public static function instance(): DeepL
+    public static function instance(): self
     {
-        return static::$instance ??= new static();
+        return self::$instance ??= new self();
     }
 
     public function translate(string $text, string $targetLanguage, string|null $sourceLanguage = null): string
