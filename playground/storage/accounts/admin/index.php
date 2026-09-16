c3650ea0ad6d4c6b73acbaf53eba1bca19378193
Johann Schopplich <johann@schopplich.com>
2026-09-16 10:31:44 +0200

    chore(playground): track the accounts

---
+++ playground/storage/accounts/admin/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'email' => 'admin@example.com',
+    'language' => 'en',
+    'name' => '',
+    'role' => 'admin'
+];
---
+++ playground/storage/accounts/playgrnd/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'email' => 'playground@example.com',
+    'language' => 'en',
+    'name' => '',
+    'role' => 'playground'
+];
